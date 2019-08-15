1d7ef9b221f05d38925574134c0194ad8b8e03d3
Robert Malecki <robmalec@umich.edu>
2019-08-15 11:44:32 -0400

    Add files via upload

--- unicodeCalc/src/unicodeCalc/unicodeCalc.java
+++ unicodeCalc/src/unicodeCalc/unicodeCalc.java
@@ -1,54 +1,41 @@
 package unicodeCalc;
 
 import java.util.Scanner;
-import java.lang.String;
 
 public class unicodeCalc {
 
 	public static void main(String[] args) {
-	
-		
+
 		String input1 = "", input2 = "";
 		int sum1 = 0, sum2 = 0;
 		Scanner scn = new Scanner(System.in);
-		
+
 		try {
-			input1 =  parseInput(scn.next());
+			input1 = parseInput(scn.next());
 			input2 = parseInput(scn.next());
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-		
+
 		sum1 = printAndSumUnicodeValOfChars(input1);
 		sum2 = printAndSumUnicodeValOfChars(input2);
-		
-		System.out.println("Difference (as absolute value): " + absoluteVal(sum1 - sum2));
+
+		System.out.println("Difference (as absolute value): " + Math.abs(sum1 - sum2));
 		scn.close();
 	}
-	public static String parseInput(String s)
-	{
+
+	public static String parseInput(String s) {
 		return s.split(":")[1];
 	}
-	public static int printAndSumUnicodeValOfChars(String s)
-	{
+
+	public static int printAndSumUnicodeValOfChars(String s) {
 		int sum = 0;
-		
-		for (char c : s.toCharArray())
-		{
-			System.out.println(c + ": " + (int)c);
-			sum += (int)c;
+
+		for (int i = 0; i < s.length(); ++i) {
+			System.out.println(s.charAt(i) + ": " + (int) s.charAt(i));
+			sum += (int) s.charAt(i);
 		}
-		
+
 		return sum;
-	}
-	public static int absoluteVal(int input)
-	{
-		if (input < 0)
-		{
-			return (-1 * input);
-		}
-		return (input);
 	}
 }
